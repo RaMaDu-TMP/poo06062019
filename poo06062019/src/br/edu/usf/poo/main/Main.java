ff408b876684c1b0aea332d622e9e15ae92402cb
Eduwardo Horibe <40374938+eduhoribe@users.noreply.github.com>
2019-05-22 18:29:17 -0300

    Add files via upload

---
+++ poo06062019/src/br/edu/usf/poo/main/Main.java
@@ -0,0 +1,12 @@
+package br.edu.usf.poo.main;
+
+import java.awt.EventQueue;
+
+public class Main {
+
+	public static void main(String[] args) {
+		EventQueue.invokeLater(() -> {
+			// TODO
+		});
+	}
+}
